40866cdcfcbc7b74ce9ba3c7335acc7592a67326
Nikolai Babuhin <nikolai.babuxin@gmail.com>
2023-11-24 12:34:59 +0300

    [FEAT] llama

--- ai/lecture_notes/llama_predict.py
+++ ai/lecture_notes/llama_predict.py
@@ -2,12 +2,24 @@ from llama_cpp import Llama
 
 llm = Llama(model_path="./llama.cpp/models/llama-2-7b-chat.ggmlv3.q4_K_M.gguf.bin", chat_format="llama-2")
 
+
 def llama_promt(termin):
     response = llm.create_chat_completion(
-        messages = [
+        messages=[
             {"role": "system", "content": "Что такое " + termin + "? одним предложением"}
         ]
     )
     return response["choices"][0]["message"]["content"]
 
+
+def llama_text_promt(termin):
+    response = llm.create_chat_completion(
+        messages=[
+            {"role": "system", "content": "Сделай более читаемым следующий текст на русском языке:" + termin}
+        ]
+    )
+    return response["choices"][0]["message"]["content"]
+
 print(llama_promt("полиморфизм"))
+
+print(llama_text_promt("Перечислите названия подразделов урока, и описание в одном предложении того, что и зачем вы в них будете рассказывать. Для этого можно воспользоваться формулировками образовательных результатов из программы, но переформулировать их на языке, понятном студенту ли обозначить темы и тезисы."))
---
+++ ai/transcriptor/STT.py
@@ -0,0 +1,23 @@
+import whisper
+
+model_type_stt="medium"
+# # Load the Whisper model
+model = whisper.load_model(model_type_stt)
+
+def transcribe_with_whisper(audio_file, model=model):
+    # Transcribe the audio
+    return model.transcribe(audio_file)
+
+def speech2text(audio_file):
+    data = transcribe_with_whisper(audio_file)
+    return {
+        'timecode_with_text': [[x['start'], x['end'], x['text']] for x in data['segments']],
+        'text': data['text']
+    }
+
+def main():
+    path_to_mp3 = ''
+    data = speech2text(path_to_mp3)
+    print(data)
+
+main()
